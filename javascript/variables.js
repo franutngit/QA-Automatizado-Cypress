2560ffca9772041e96a0bc0794fc27d8437fb2fc
vGoen <francoezequielbarroso@gmail.com>
2024-01-23 14:45:17 -0300

    variables

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,3 +1,26 @@
-let cadena = "asd";
+//Tipos de Declaracion de variables
+var variable1 = "hola";
+let variable2 = "mundo";
+const variable3 = 1;
 
-console.log(asd);
+//Cadena
+let variable4 = "fruta";
+console.log("Variable 4: "+variable4); //fruta
+
+//Nuemérica
+variable4 = 5.5;
+console.log("Variable 4: "+variable4); //5.5
+
+//Objects
+let obj =["A","B","C"];
+let obj2 = {a:"A",b:"B",c:"C"};
+console.log(obj[1]);
+console.log(obj2.c);
+
+//TypeOf
+let cadena = "Arbol";
+let numerica = 7;
+let array = ["asd",2,3];
+typeof cadena
+typeof numerica
+typeof array
