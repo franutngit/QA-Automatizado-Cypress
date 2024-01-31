9bb740de5c01e89b28d08291458c39a71d573977
vGoen <francoezequielbarroso@gmail.com>
2024-01-31 13:46:36 -0300

    selectores

--- cypress/e2e/Selectores.cy.js
+++ cypress/e2e/Selectores.cy.js
@@ -4,6 +4,7 @@ describe('Curso Cypress - Capitulo 6', () => {
         cy.viewport(1920, 1080);
         cy.visit('https://demoqa.com/text-box').wait(2000);
         cy.get('#userName').type('Juan Martinez').wait(1000);
+        //cy.xpath('//*[@id="userName"]').type('Juan Martinez').wait(1000);
         cy.get('#userEmail').type('juanmartinez@mailinator.com').wait(1000);
         cy.get('#currentAddress').type('123 Calle San Martín').wait(1000);
         cy.get('#permanentAddress').type('Casa');
